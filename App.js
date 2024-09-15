6edd43980884d713144b15f21e591f495efc6732
lukman bolaji <bolajiakanji21@gmail.com>
2024-09-15 02:19:25 -0700

    delete apptext and add many components

--- App.js
+++ App.js
@@ -1,56 +1,139 @@
+// import { StyleSheet } from "react-native";
+// import AppText from "./components/AppText";
+// import Screen from "./components/Screen";
 
-import { Platform, StyleSheet, View,Text } from 'react-native';
- import { StatusBar } from 'expo-status-bar';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import AppText from './components/AppText';
-import Constants from 'expo-constants'
-import Screen from './components/Screen';
+// export default function App() {
+//   return (
+//     <Screen style={styles.container} statusBarColor="light">
+//       <AppText style={{ color: "white" }}>
+//         dfgsghhsfhsgdvcsvvvvvxvvverhbolajilukman
+//       </AppText>
+//     </Screen>
+//   );
+// }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "purple",
+//     flex: 1,
+//   },
+// });
+
+
+// import { useState } from 'react';
+// import { Button, Image, View, StyleSheet } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+// import Screen from './components/Screen';
+
+// export default function ImagePickerExample() {
+//   const [image, setImage] = useState('');
+
+//   const pickImage = async () => {
+//     // No permissions request is necessary for launching the image library
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ImagePicker.MediaTypeOptions.All,
+//       allowsEditing: true,
+//       aspect: [4, 3],
+//       quality: 1,
+//     });
+
+//     console.log(result);
+
+//     if (!result.canceled) {
+//       setImage(result.assets[0].uri);
+//     }
+//   };
+
+//   return (
+//     <Screen style={styles.container}>
+    
+//       <Button title="Pick an image from camera roll" onPress={pickImage} />
+//       {image && <Image source={{ uri: image }} style={styles.image} />}
+      
+//       </Screen>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'yellow'
+//   },
+//   image: {
+//     width: 200,
+//     height: 200,
+//   },
+// });
+
+
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
-  
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
-    
-    
-        
-    < Screen style={{backgroundColor: 'purple', flex: 1}} statusBarColor='light'>
-    <AppText style={{color: 'white'}}>dfgsghhsfhsgdvcsvvvvvxvvverhbolaji</AppText>
-    </Screen >
-   
-      
-  )
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50 + Constants.statusBarHeight ,
-
-    
-    height: 50,
-    backgroundColor: 'purple',
-    
-   // paddingLeft: 220,
-    justifyContent: 'space-between',
-    alignItems: '',
-    flexDirection: 'row'
-    
-
-    
-
-    
-    
-    
-    
-    
+    flex: 1,
+    justifyContent: 'center',
   },
-
-  mine: {
-    width: 50,
-    height: 50,
-    borderRadius: 35,
-    backgroundColor: 'yellow',
-    paddingLeft: 20,
-  }
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
