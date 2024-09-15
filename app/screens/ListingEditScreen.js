d8ab7f8d049676ea14816f94ea8cb838dcd6a2fd
lukman bolaji <bolajiakanji21@gmail.com>
2024-09-15 06:45:19 -0700

    Remove image upload from form

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -11,16 +11,16 @@ import {
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
 import FormImagePicker from "../components/forms/FormImagePicker";
-import listingsApi from "../api/listings";
-import useLocation from "../hooks/useLocation";
-import UploadScreen from "./UploadScreen";
+// import listingsApi from "../api/listings";
+// import useLocation from "../hooks/useLocation";
+// import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
-  images: Yup.array().min(1, "Please select at least one image."),
+  // images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
@@ -81,44 +81,44 @@ const categories = [
 ];
 
 function ListingEditScreen() {
-  const location = useLocation();
-  const [uploadVisible, setUploadVisible] = useState(false);
-  const [progress, setProgress] = useState(0);
+  // const location = useLocation();
+  // const [uploadVisible, setUploadVisible] = useState(false);
+  // const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing, { resetForm }) => {
-    setProgress(0);
-    setUploadVisible(true);
-    const result = await listingsApi.addListing(
-      { ...listing, location },
-      (progress) => setProgress(progress)
-    );
+  // const handleSubmit = async (listing, { resetForm }) => {
+  //   setProgress(0);
+  //   setUploadVisible(true);
+  //   const result = await listingsApi.addListing(
+  //     { ...listing, location },
+  //     (progress) => setProgress(progress)
+  //   );
 
-    if (!result.ok) {
-      setUploadVisible(false);
-      return alert("Could not save the listing");
-    }
-    resetForm();
-  };
+  //   if (!result.ok) {
+  //     setUploadVisible(false);
+  //     return alert("Could not save the listing");
+  //   }
+  //   resetForm();
+  // };
 
   return (
     <Screen style={styles.container}>
-      <UploadScreen
+      {/* <UploadScreen
         onDone={() => setUploadVisible(false)}
         progress={progress}
         visible={uploadVisible}
-      />
+      /> */}
       <Form
         initialValues={{
           title: "",
           price: "",
           description: "",
           category: null,
-          images: [],
+          // images: [],
         }}
-        onSubmit={handleSubmit}
+        onSubmit={console.log('submit') }
         validationSchema={validationSchema}
       >
-        <FormImagePicker name="images" />
+        {/* <FormImagePicker name="images" /> */}
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
