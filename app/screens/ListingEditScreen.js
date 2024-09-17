9ac1888bc0202987c045b54c88e6841fa7339263
lukman bolaji <bolajiakanji21@gmail.com>
2024-09-16 23:24:38 -0700

    Build listing edit screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -11,9 +11,6 @@ import {
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
 import FormImagePicker from "../components/forms/FormImagePicker";
-// import listingsApi from "../api/listings";
-// import useLocation from "../hooks/useLocation";
-// import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -81,33 +78,10 @@ const categories = [
 ];
 
 function ListingEditScreen() {
-  // const location = useLocation();
-  // const [uploadVisible, setUploadVisible] = useState(false);
-  // const [progress, setProgress] = useState(0);
-
-  // const handleSubmit = async (listing, { resetForm }) => {
-  //   setProgress(0);
-  //   setUploadVisible(true);
-  //   const result = await listingsApi.addListing(
-  //     { ...listing, location },
-  //     (progress) => setProgress(progress)
-  //   );
-
-  //   if (!result.ok) {
-  //     setUploadVisible(false);
-  //     return alert("Could not save the listing");
-  //   }
-  //   resetForm();
-  // };
-
+  
   return (
     <Screen style={styles.container} statusBarColor='light'>
-      {/* <UploadScreen
-        onDone={() => setUploadVisible(false)}
-        progress={progress}
-        visible={uploadVisible}
-      /> */}
-      <Form
+            <Form
         initialValues={{
           title: "",
           price: "",
@@ -140,7 +114,7 @@ function ListingEditScreen() {
           multiline
           name="description"
           numberOfLines={3}
-          placeholder="Description"5
+          placeholder="Description"
         />
         <SubmitButton title="Post" />
       </Form>
