c28cc5b918ea966b834418d7ea284935ba7b5eed
lukman bolaji <bolajiakanji21@gmail.com>
2024-09-17 23:40:44 -0700

    Build auth-navigator

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ListingEditScreen from './app/screens/ListingEditScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import AuthNavigator from './app/navigation/AuthNavigator'
 
 const App = () => {
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
   )
 }
 
