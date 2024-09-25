3e751f4e524b4afa3800f74057cf584b09f64af1
lukman bolaji <bolajiakanji21@gmail.com>
2024-09-25 01:26:41 -0700

    Build offline support

--- app/components/OfflineNotice.js
+++ app/components/OfflineNotice.js
@@ -2,19 +2,34 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import AppText from './Text'
 import colors from '../config/colors'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNetInfo } from '@react-native-community/netinfo'
 
 export default function OfflineNotice() {
+    const netInfo = useNetInfo()
+
+    if (netInfo.isInternetReachable === false && netInfo.type !== 'unknown')
   return (
-    <View style={styles.container}>
-      <AppText>No internet connection</AppText>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <AppText style={styles.text}>No internet connection</AppText>
+    </SafeAreaView>
+        )
+    
+    return null
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.primary,
         height: 50,
-        position: 'absolute'
+        position: 'absolute',
+        zIndex: 3,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    text: {
+        color: colors.white
+
     }
 })
