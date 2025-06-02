696a6103ed0b71a6f8119d824bf99ca14274b470
lukman bolaji <bolajiakanji21@gmail.com>
2025-06-02 05:29:27 +0100

    Change like color

--- app/utility/likesColor.js
+++ app/utility/likesColor.js
@@ -2,7 +2,7 @@ import colors from "../config/colors"
 
 const getLikesColor = (user, likesArray) => {
     const isUserLikes = likesArray?.find(userLike => userLike === user)
-  return  isUserLikes ? colors.primary: '#000'
+  return  isUserLikes ? colors.primary: '#1a8cff'
 }
 
 export default getLikesColor
