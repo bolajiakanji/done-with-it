31831bb9b13f106d6b9002e18c1d0f306a4dcad8
Lukman Bolaji <bolajiakanji21@gmail.com>
2025-08-22 09:38:18 -0700

    Create listing footer

---
+++ app/components/ListingsFooter.js
@@ -0,0 +1,33 @@
+import { Text, } from "react-native";
+
+
+const ListingsFooter = ({ displayItems, isLoading, listingsQueryObject }) => {
+    if (isLoading && displayItems?.length > 0) {
+        return (
+            <Text style={{ textAlign: "center", paddingBottom: 10, backgroundColor: '#e6f2ff' }}>
+                Loading...</Text>
+        );
+    }
+    if (!isLoading && !listingsQueryObject.page && displayItems.length === 0) {
+        return (
+            <Text style={{
+                textAlign: "center", paddingBottom: 10, marginTop: 20
+
+            }}>
+                No Data
+            </Text>
+        );
+    }
+    if (!isLoading && !listingsQueryObject.page) {
+        return (
+            <Text style={{
+                textAlign: "center", paddingBottom: 10,
+            }}>
+                No more Data
+            </Text>
+        );
+    }
+
+}
+
+export default ListingsFooter
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, RefreshControl, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, RefreshControl, View } from "react-native";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
@@ -7,6 +7,7 @@ import Screen from "../components/Screen";
 import myCloud from "../utility/cid";
 import Flatlist_header from "../components/Flatlist_header";
 import useListings from "../hooks/useListings";
+import ListingsFooter from "../components/ListingsFooter";
 
 function ListingsScreen({ navigation }) {
   const [refreshing, setRefreshing] = useState(false);
@@ -14,16 +15,15 @@ function ListingsScreen({ navigation }) {
   const [listingsQueryObject, setListingsQueryObject] = useState({});
   const [displayItems, setDisplayItems] = useState([]);
 
-  const { request, error, loading, setData, loadListings,loadListings_2 } =
+  const { request, error, loading, setData, loadListings, loadListings_2 } =
     useListings(
       listingsQueryObject,
       setListingsQueryObject,
       setDisplayItems,
       setIsLoading
-);
+    );
+
   const cld = myCloud()
-
-
 
   const onEndReached = () => {
     if (!isLoading && listingsQueryObject.page) {
@@ -31,32 +31,16 @@ function ListingsScreen({ navigation }) {
     }
   };
 
-  const listFooterComponent = () => {
-    if (isLoading && displayItems?.length > 0) {
-      return (
-        <Text style={{ textAlign: "center", paddingBottom: 10, backgroundColor: '#e6f2ff' }}>Loading...</Text>
-      );
-    }
-    if (!isLoading && !listingsQueryObject.page && displayItems.length === 0) {
-      return (
-        <Text style={{
-          textAlign: "center", paddingBottom: 10, marginTop: 20
+  const listingsFooterComponent = () => {
+    return (
+      <ListingsFooter
+        displayItems={displayItems}
+        listingsQueryObject={listingsQueryObject}
+        isLoading={isLoading}
+      />
+    )
+  }
 
-        }}>
-          No Data
-        </Text>
-      );
-    }
-    if (!isLoading && !listingsQueryObject.page) {
-      return (
-        <Text style={{
-          textAlign: "center", paddingBottom: 10,
-        }}>
-          No more Data
-        </Text>
-      );
-    }
-  };
   const Header = () => {
     return (
       <Flatlist_header
@@ -79,42 +63,36 @@ function ListingsScreen({ navigation }) {
   };
 
   return (
-    <>
-      <Screen style={styles.screen} barStyle='light-content' background={colors.primary}  >
-        < View style={{ position: 'absolute', zIndex: 0, height: 100, backgroundColor: colors.primary, width: '100%' }}></View>
-        <FlatList
-          data={displayItems}
-          keyExtractor={(listing, index) => index}
 
-          renderItem={({ item }) => {
-            const myImage = cld.image(item.images[0]);
-            return (
-              <Card
-                item={item}
-                myImage={myImage}
-                onPress={() =>
-                  navigation.navigate(routes.LISTING_DETAILS, item)
-                }
-              />
-            );
-          }}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
-          onEndReached={onEndReached}
-          //ListEmptyComponent={listEmptyComponent}
-          ListFooterComponent={listFooterComponent}
-          ListHeaderComponent={Header}
-          stickyHeaderIndices={[0]}
-          initialNumToRender={10}
-
-          numColumns="2"
-          columnWrapperStyle={{ columnGap: 10, paddingTop: 10, paddingHorizontal: 10, backgroundColor: '#e6f2ff' }}
-        />
-
-
-      </Screen>
-    </>
+    <Screen style={styles.screen} barStyle='light-content' background={colors.primary}  >
+      < View style={{ position: 'absolute', zIndex: 0, height: 100, backgroundColor: colors.primary, width: '100%' }}></View>
+      <FlatList
+        data={displayItems}
+        keyExtractor={(listing,index) => index}
+        renderItem={({ item }) => {
+          const myImage = cld.image(item.images[0]);
+          return (
+            <Card
+              item={item}
+              myImage={myImage}
+              onPress={() =>
+                navigation.navigate(routes.LISTING_DETAILS, item)
+              }
+            />
+          );
+        }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        onEndReached={onEndReached}
+        ListFooterComponent={listingsFooterComponent}
+        ListHeaderComponent={Header}
+        stickyHeaderIndices={[0]}
+        initialNumToRender={10}
+        numColumns="2"
+        columnWrapperStyle={{ columnGap: 10, paddingTop: 10, paddingHorizontal: 10, backgroundColor: '#e6f2ff' }}
+      />
+    </Screen>
   );
 }
 
